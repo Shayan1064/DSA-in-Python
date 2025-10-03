05b0eb91231c5bcc5dc97ba8ee03bdfc75922d9b
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-10-03 23:46:35 +0500

    What is Time & Space Complixity Detailed Explanation

---
+++ 1_Time&Space_Complixity.py
@@ -0,0 +1,28 @@
+# Time complexity shows how efficient your code is when data grows.
+# What is O(N) and what does it mean...
+
+# O = Worst case (upper bound).
+# Ω = Best case (lower bound).
+# Θ = Average case (tight bound).
+
+# for i in range(n):        # Outer loop
+#     for j in range(n):    # Inner loop
+#         print(i, j)
+
+# Outer loop runs n times.
+
+# For each run of the outer loop, the inner loop also runs n times.
+
+# So total = n × n = n² iterations.
+
+#------------------------------------------------------------------------
+
+# Space Complexity → Total memory used by an algorithm (input + extra).
+
+# Input Space → Memory required to store the input data.
+
+# Auxiliary Space → Extra memory used by the algorithm (temporary variables, arrays, etc.).
+
+#-----------------------------------------------------------------------------------------------
+
+# Server do 10 power 8 operation in one second if it exceed then it is called Time Limit Exceeded.
